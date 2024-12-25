a7684138bc19e36c5698c7fe43a9c6240afe1995
Shawn Hou <shawnhouforwork@gmail.com>
2024-12-25 10:24:23 +0800

    init

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,24 @@
+with source as (
+    
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+    select * from {{ ref('raw_payments') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        order_id,
+        payment_method,
+
+		amount
+
+    from source
+
+)
+
+select * from renamed
